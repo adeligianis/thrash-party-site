2dd03fa4133bece3f04adac1d951565f7da9b824
Adam Deligianis <adamdeligianis@Adams-MacBook-Air.local>
2026-07-05 22:11:12 -0400

    seo updates and optimization

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        neonBlue: '#01ffff',
+        neonPink: '#ff00ff',
+      }
+    }
+  },
+  plugins: [],
+}
